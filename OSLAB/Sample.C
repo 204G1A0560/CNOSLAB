90b87af48cee8113927aae594b8f1e3511ff1cb3
Mounesh <94514583+204G1A0560@users.noreply.github.com>
2022-08-30 11:15:37 +0530

    Sample.C
    G.Mounesh completed Sample.c with C source Code

---
+++ OSLAB/Sample.C
@@ -0,0 +1,12 @@
+/* Name Of the Candidate: G.Mounesh
+   Roll No: 204G1A0560
+   Title of the Experiment: Sample Program using C
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
+
+// Source Code
+    #include <stdio.h>
+    void main(){
+	printf("Hello World\n");
+ }
